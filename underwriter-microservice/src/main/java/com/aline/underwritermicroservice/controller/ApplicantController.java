cd256472dd3b48ce781086fa720c9242e8bd9669
leandroyabut <leandro.yabut@smoothstack.com>
2021-07-20 15:48:01 -0600

    Fix phone number validation regexp.

--- underwriter-microservice/src/main/java/com/aline/underwritermicroservice/controller/ApplicantController.java
+++ underwriter-microservice/src/main/java/com/aline/underwritermicroservice/controller/ApplicantController.java
@@ -2,26 +2,39 @@ package com.aline.underwritermicroservice.controller;
 
 import com.aline.core.dto.CreateApplicantDTO;
 import com.aline.core.model.Applicant;
+import com.aline.underwritermicroservice.service.ApplicantService;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
+import java.net.URI;
 
 @RestController
 @RequestMapping("/applicants")
+@RequiredArgsConstructor
 @Slf4j(topic = "Applicants")
 public class ApplicantController {
 
+    private final ApplicantService service;
+
     @ApiOperation("Create an Applicant")
     @PostMapping
-    public ResponseEntity<Applicant> createApplicant(@RequestBody @Valid CreateApplicantDTO applicant) {
-        return ResponseEntity.ok().build();
+    public ResponseEntity<Applicant> createApplicant(@RequestBody @Valid CreateApplicantDTO createApplicantDTO) {
+        Applicant applicant = service.createApplicant(createApplicantDTO);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentContextPath()
+                .path("/{id}")
+                .buildAndExpand(applicant.getId())
+                .toUri();
+        return ResponseEntity.created(location).body(applicant);
     }
 
 }
--- underwriter-microservice/src/main/java/com/aline/underwritermicroservice/service/ApplicantService.java
+++ underwriter-microservice/src/main/java/com/aline/underwritermicroservice/service/ApplicantService.java
@@ -1,7 +1,22 @@
 package com.aline.underwritermicroservice.service;
 
+import com.aline.core.dto.CreateApplicantDTO;
+import com.aline.core.model.Applicant;
+import com.aline.core.repository.ApplicantRepository;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class ApplicantService {
+
+    private final ApplicantRepository repository;
+    private final ModelMapper mapper;
+
+    public Applicant createApplicant(CreateApplicantDTO createApplicantDTO) {
+        Applicant applicant = mapper.map(createApplicantDTO, Applicant.class);
+        return repository.save(applicant);
+    }
+
 }
