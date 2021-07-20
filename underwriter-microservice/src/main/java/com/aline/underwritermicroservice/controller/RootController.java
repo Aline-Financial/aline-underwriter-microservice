86f00e49f03a0f3d79dfcce15e24a7f385ccb7a9
leandroyabut <leandro.yabut@smoothstack.com>
2021-07-19 20:54:31 -0600

    Use api annotations from Swagger to create API docs.

--- underwriter-microservice/src/main/java/com/aline/underwritermicroservice/controller/RootController.java
+++ underwriter-microservice/src/main/java/com/aline/underwritermicroservice/controller/RootController.java
@@ -1,5 +1,9 @@
 package com.aline.underwritermicroservice.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,13 +12,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class RootController {
 
     /**
-     * <strong>Health Check endpoint</strong>
-     * <p>Endpoint: <code>/health</code></p>
-     * <p>Status <code>200 OK</code> if the application is running.</p>
+     * Health check endpoint
+     * <p><strong>Endpoint: </strong> <code>/health</code></p>
+     * <p><code>Status 200 OK</code> if the application is running.</p>
      *
      * @return {@link ResponseEntity<Void>} No content.
      */
     @GetMapping("/health")
+    @ApiOperation("Health check endpoint")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Service is healthy.") ,
+            @ApiResponse(code = 404, message = "The service is probably not running.")
+    })
     public ResponseEntity<Void> healthCheck() {
         return ResponseEntity.ok().build();
     }
