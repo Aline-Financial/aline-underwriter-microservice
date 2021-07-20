d644abf0ac2c8e6397e993042bae4ee74b70f5d9
leandroyabut <leandro.yabut@smoothstack.com>
2021-07-20 12:57:15 -0600

    Add group to gender.

--- underwriter-microservice/src/main/java/com/aline/underwritermicroservice/controller/RootController.java
+++ underwriter-microservice/src/main/java/com/aline/underwritermicroservice/controller/RootController.java
@@ -1,6 +1,5 @@
 package com.aline.underwritermicroservice.controller;
 
-import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
@@ -11,13 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RootController {
 
-    /**
-     * Health check endpoint
-     * <p><strong>Endpoint: </strong> <code>/health</code></p>
-     * <p><code>Status 200 OK</code> if the application is running.</p>
-     *
-     * @return {@link ResponseEntity<Void>} No content.
-     */
     @GetMapping("/health")
     @ApiOperation("Health check endpoint")
     @ApiResponses({
