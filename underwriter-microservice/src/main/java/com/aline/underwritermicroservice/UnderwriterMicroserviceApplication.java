1e078e7128191d70ee44de8be981d31bea6cdddf
leandroyabut <leandro.yabut@smoothstack.com>
2021-08-27 16:32:45 -0600

    AF-258 Fix formatting in main application.

--- underwriter-microservice/src/main/java/com/aline/underwritermicroservice/UnderwriterMicroserviceApplication.java
+++ underwriter-microservice/src/main/java/com/aline/underwritermicroservice/UnderwriterMicroserviceApplication.java
@@ -8,13 +8,11 @@ import org.springframework.boot.SpringApplication;
 
 @SpringBootApplication
 @EnableCoreModule
-@OpenAPIDefinition(info =
-        @Info(
-                title = "Underwriter Microservice API",
-                description = "This API provides operations to either accept or deny an application for an account as well as Applicant and Member creation and management.",
-                version = "1.0"
-        )
-)
+@OpenAPIDefinition(info = @Info(
+    title = "Underwriter Microservice API",
+    description = "This API provides operations to either accept or deny an application for an account as well as Applicant and Member creation and management.",
+    version = "1.0"
+))
 public class UnderwriterMicroserviceApplication {
 
     public static void main(String[] args) {
