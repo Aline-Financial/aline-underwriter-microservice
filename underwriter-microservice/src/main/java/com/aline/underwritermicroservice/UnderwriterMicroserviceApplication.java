18c3a096ac40d645c7fe9b4538734eba5e09613b
leandroyabut <leandro.yabut@smoothstack.com>
2021-08-27 16:30:22 -0600

    AF-258 Add enable core module annotation.

--- underwriter-microservice/src/main/java/com/aline/underwritermicroservice/UnderwriterMicroserviceApplication.java
+++ underwriter-microservice/src/main/java/com/aline/underwritermicroservice/UnderwriterMicroserviceApplication.java
@@ -1,17 +1,13 @@
 package com.aline.underwritermicroservice;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.aline.core.annotations.EnableCoreModule;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan("com.aline")
-@EnableJpaRepositories("com.aline.core.repository")
-@EntityScan("com.aline.core.model")
+@EnableCoreModule
 @OpenAPIDefinition(info =
         @Info(
                 title = "Underwriter Microservice API",
