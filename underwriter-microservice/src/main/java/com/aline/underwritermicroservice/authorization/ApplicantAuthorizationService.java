f85cec81daff1cd500563ebea19f582713939feb
leandroyabut <leandro.yabut@smoothstack.com>
2021-08-29 18:48:48 -0600

    AF-258 Update applicant authorization service to use the new getUser feature.

--- underwriter-microservice/src/main/java/com/aline/underwritermicroservice/authorization/ApplicantAuthorizationService.java
+++ underwriter-microservice/src/main/java/com/aline/underwritermicroservice/authorization/ApplicantAuthorizationService.java
@@ -1,28 +1,27 @@
 package com.aline.underwritermicroservice.authorization;
 
 import com.aline.core.model.Applicant;
-import com.aline.core.repository.ApplicantRepository;
+import com.aline.core.model.user.MemberUser;
+import com.aline.core.model.user.UserRole;
 import com.aline.core.security.service.AbstractAuthorizationService;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.Objects;
-import java.util.Optional;
 
 @Component("applicantAuth")
 @RequiredArgsConstructor
 public class ApplicantAuthorizationService extends AbstractAuthorizationService<Long> {
 
-    private final ApplicantRepository repository;
-
     @Override
     public boolean canAccess(@NonNull Long id) {
-        Optional<Applicant> applicantOptional = repository.findApplicantByUsername(getUsername());
-        if (applicantOptional.isPresent()) {
-            Applicant applicant = applicantOptional.get();
-            return (Objects.equals(applicant.getId(), id) || roleIsManagement());
+        if (getUser().getUserRole() == UserRole.MEMBER) {
+            MemberUser user = (MemberUser) getUser();
+            Applicant applicant = user.getMember().getApplicant();
+            return Objects.equals(applicant.getId(), id);
         }
-        return false;
+
+        return roleIsManagement();
     }
 }
